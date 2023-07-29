1446679792714b14886717a7f2a0ba4149b4fb1b
possiblyHugo <alainhandzar@gmail.com>
2023-07-28 19:43:15 -0700

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	// Vars
+	int first_number, second_number;
+	first_number, second_number = 0, 0;
+
+	// Start
+	std::cout << "Multi-digit multiplication calculator." << std::endl;
+	
+	// Inputs
+	std::cout << "Please input the first number: " << std::endl;
+	std::cin >> first_number;
+
+	std::cout << "Please input the second number: " << std::endl;
+	std::cin >> second_number;
+
+	// Display
+
+
+	return 0;
+}
