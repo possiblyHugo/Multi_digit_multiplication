1bd5b4d0d609a13da47ab697740c3b75004243e5
possiblyHugo <alainhandzar@gmail.com>
2023-07-28 23:59:22 -0700

    + hash map to contain data and values

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream> // std::cout, std:cin
 #include <vector> // std::vector
-#include <algorithm> // std::reverse
+//#include <algorithm> // std::reverse
+#include <unordered_map> // std::unordered_map
+
 
 // Functions
 std::vector<int> get_digits(int number);
@@ -25,19 +27,44 @@ int main()
 	std::vector<int> first_number_split = get_digits(first_number);
 	std::vector<int> second_number_split = get_digits(second_number);
 
-	// Multiply first row
+	// Hash map
+	int rows_amount = second_number_split.size();
+	std::unordered_map<int, std::vector<int>> rows_data;
+
+	// Multiplication
+	bool carry_number = false;
 
 	for (int num2 = 0; num2 < second_number_split.size(); num2++) // Row 1 (first number)
 	{
 		for (int num1 = 0; num1 < first_number_split.size(); num1++) // Row 2 (second number)
 		{
-			std::cout << second_number_split[num2] << " * " << first_number_split[num1] << " " << first_number_split[num1] * second_number_split[num2] << std::endl;
+			int result = second_number_split[num2] * first_number_split[num1];
+			
+			if (carry_number) // If number previous was > 10, add 1 to account for carrying digit
+			{
+				result++;
+				carry_number = false;
+			}
+			else if (result >= 10)
+			{
+				carry_number = true;
+			}
+
+			rows_data[num2].push_back(result);
+
 		}
 	}
 
 	// Display
+	for (int row = 0; row < rows_amount; row++)
+	{
+		for (int number = 0; number < rows_data[row].size(); number++)
+		{
+			std::cout << rows_data[row][number] << " ";
 
-
+		}
+		std::endl;
+	}
 
 	return 0;
 }
